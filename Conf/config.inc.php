cd71dd40bf3b028d2433c4da96c84d9fe90e7b5b
suziewong <monkeysuzie@gmail.com>
2013-05-06 10:24:17 +0800

    config.inc.php

--- Conf/config.inc.php
+++ Conf/config.inc.php
@@ -5,25 +5,13 @@ return array(
 	'URL_MODEL'=>  0, 			// 0普通模式，1.pathinfo[利于SEO] 如果你的环境不支持PATHINFO 请设置为3
 	'DB_TYPE'   => 'mysql', 	// 数据库类型
 	'DB_HOST'   => '', // 服务器地址
-	'DB_NAME'   => 'feel', 		// 数据库名
+	'DB_NAME'   => '', 		// 数据库名
 	'DB_USER'   => '', 	// 用户名
 	'DB_PWD'    => '', 	// 密码
 	'DB_PORT'   => 3306, 		// 端口
-	'DB_PREFIX' => 'feel_', 	// 数据库表前缀
+	'DB_PREFIX' => 'xx_', 	// 数据库表前缀
 
 
-    // 'DEFAULT_TIMEZONE'   => 'PRC',	// 默认时区
-	//'HOME_TMPL_PATH' 	 => '../Home/Template/', //定义前台项目模板存放路径
-	'HOME_DEFAULT_THEME' => 'default', // 定义默认模板主题名称
-	//'DEFAULT_THEME' => 'default',
-	//'THEME_LIST'		=>	'default,think',
-    //'TMPL_DETECT_THEME' => 	true, // 自动侦测模板主题
-
-	'FACE_UPLOAD_PATH' => './Common/Uploads/DJ/Face/', //定义后台Face上传文件存放路径
-	'HEAD_UPLOAD_PATH' => './Common/Uploads/DJ/Head/', //定义后台Head上传文件存放路径
-    'MP3_UPLOAD_PATH'  =>  './Common/Uploads/MP3/', 		//定义后台MP3上传文件存放路径
-    'LIFE_UPLOAD_PATH' => './Common/Uploads/DJ/Life/', //定义后台生活照上传文件存放路径
-	'UPLOAD_FILE_SIZE' => 1024*1024*50, 				//定义后台上传文件附件大小  10M
 
 	
 	'TMPL_ACTION_ERROR' => 'Public:error',			//默认错误跳转对应的模板文件	
