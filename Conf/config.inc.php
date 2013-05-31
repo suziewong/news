fbf73560876ddd9c6f16bbd8da615a16681d563b
suziewong <monkeysuzie@gmail.com>
2013-05-31 19:20:24 +0800

    del

--- Conf/config.inc.php
+++ Conf/config.inc.php
@@ -16,7 +16,6 @@ return array(
 	
 	'TMPL_ACTION_ERROR' => 'Public:error',			//默认错误跳转对应的模板文件	
 	'TMPL_ACTION_SUCCESS' => 'Public:success',		//默认成功跳转对应的模板文件
-
 	'TMPL_PARSE_STRING'  =>array(
     	'__PUBLIC__' => __ROOT__.'/Common', 		// 更改默认的__PUBLIC__ 替换规则
      	'__ADMIN__' => __ROOT__.'/Common/', 	// 更改默认的__PUBLIC__ 替换规则
